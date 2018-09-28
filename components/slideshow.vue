c0567f1d5bbe1e319fb1d172f9c674cd583b5dec
phinemo <danarwidiutomo@gmail.com>
2018-09-28 08:52:45 +0700

    (Temp) remove overlay

--- components/slideshow.vue
+++ components/slideshow.vue
@@ -4,10 +4,10 @@
                 <ul class="uk-slideshow-items">
                     <li v-for="(cover,index) in covers" :key="index" class="uk-transition-toggle">
                         <img :src="cover.url" alt="" uk-cover>
-                        <div class="uk-hidden-hover uk-transition-slide-bottom uk-overlay uk-padding-small uk-overlay-primary uk-position-bottom uk-text-center">
+                        <!-- <div class="uk-hidden-hover uk-transition-slide-bottom uk-overlay uk-padding-small uk-overlay-primary uk-position-bottom uk-text-center">
                         <h3 class="uk-margin-remove">Overlay Bottom</h3>
                         <p class="uk-margin-remove">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </div>
+                        </div> -->
                     </li>
                 </ul>
                 <a class="uk-position-center-left uk-position-small " href="#" uk-slidenav-previous
