2c76ac4f043d0b83048e03baae25fb8e5906adcc
phinemo <danarwidiutomo@gmail.com>
2018-09-28 09:19:29 +0700

    migrate file and some fix

---
+++ components/listbycategory/cardlist.vue
@@ -0,0 +1,108 @@
+<template>
+  
+    <div>
+        
+          <hr class="uk-margin-remove-top uk-margin-medium-bottom ">
+          <div class="uk-text-center">
+                <h3 class="uk-text-bold">Daftar Penawaran Spesial</h3>
+         </div>
+         <hr class="uk-margin-remove-top uk-margin-small-bottom">
+       
+            <div class="uk-card uk-card-default">
+                                <div class="uk-card-media-top">
+                                    <img class="uk-width-1-1 uk-height-max-medium" src="https://via.placeholder.com/600x300" uk-height-viewport="offset-top: true; offset-bottom: 100" alt="">
+                                </div>
+                        <div class="uk-card-body">
+                                    <h6 class="uk-card-title uk-text-bold" style="font-size: 18px;">Judul Penawaran 1</h6>
+                                      <p class="uk-margin-remove-bottom">Mulai Dari Harga</p>
+                                      <span uk-icon="icon: star; ratio: 0.8"></span>
+                                      <span uk-icon="icon: star; ratio: 0.8"></span>
+                                      <span uk-icon="icon: star; ratio: 0.8"></span>
+                                      <span uk-icon="icon: star; ratio: 0.8"></span>
+                                      <span uk-icon="icon: star; ratio: 0.8"></span>
+                        </div>
+            </div>
+    
+        <hr class="uk-margin-remove-top uk-margin-small-bottom">
+       
+            <div class="uk-card uk-card-default">
+                                <div class="uk-card-media-top">
+                                    <img class="uk-width-1-1 uk-height-max-medium" src="https://via.placeholder.com/600x300" uk-height-viewport="offset-top: true; offset-bottom: 100" alt="">
+                                </div>
+                        <div class="uk-card-body">
+                                    <h6 class="uk-card-title uk-text-bold" style="font-size: 18px;">Judul Penawaran 2</h6>
+                                      <p class="uk-margin-remove-bottom">Mulai Dari Harga</p>
+                                      <span uk-icon="icon: star; ratio: 0.8"></span>
+                                      <span uk-icon="icon: star; ratio: 0.8"></span>
+                                      <span uk-icon="icon: star; ratio: 0.8"></span>
+                                      <span uk-icon="icon: star; ratio: 0.8"></span>
+                                      <span uk-icon="icon: star; ratio: 0.8"></span>
+                        </div>
+            </div>
+
+            <hr class="uk-margin-remove-top uk-margin-small-bottom">
+       
+            <div class="uk-card uk-card-default">
+                                <div class="uk-card-media-top">
+                                    <img class="uk-width-1-1 uk-height-max-medium" src="https://via.placeholder.com/600x300" uk-height-viewport="offset-top: true; offset-bottom: 100" alt="">
+                                </div>
+                        <div class="uk-card-body">
+                                    <h6 class="uk-card-title uk-text-bold" style="font-size: 18px;">Judul Penawaran 3</h6>
+                                      <p class="uk-margin-remove-bottom">Mulai Dari Harga</p>
+                                      <span uk-icon="icon: star; ratio: 0.8"></span>
+                                      <span uk-icon="icon: star; ratio: 0.8"></span>
+                                      <span uk-icon="icon: star; ratio: 0.8"></span>
+                                      <span uk-icon="icon: star; ratio: 0.8"></span>
+                                      <span uk-icon="icon: star; ratio: 0.8"></span>
+                        </div>
+            </div>
+
+            <hr class="uk-margin-remove-top uk-margin-small-bottom">
+       
+            <div class="uk-card uk-card-default">
+                                <div class="uk-card-media-top">
+                                    <img class="uk-width-1-1 uk-height-max-medium" src="https://via.placeholder.com/600x300" uk-height-viewport="offset-top: true; offset-bottom: 100" alt="">
+                                </div>
+                        <div class="uk-card-body">
+                                    <h6 class="uk-card-title uk-text-bold" style="font-size: 18px;">Judul Penawaran 4</h6>
+                                      <p class="uk-margin-remove-bottom">Mulai Dari Harga</p>
+                                      <span uk-icon="icon: star; ratio: 0.8"></span>
+                                      <span uk-icon="icon: star; ratio: 0.8"></span>
+                                      <span uk-icon="icon: star; ratio: 0.8"></span>
+                                      <span uk-icon="icon: star; ratio: 0.8"></span>
+                                      <span uk-icon="icon: star; ratio: 0.8"></span>
+                        </div>
+            </div>
+
+            <hr class="uk-margin-remove-top uk-margin-small-bottom">
+       
+            <div class="uk-card uk-card-default">
+                                <div class="uk-card-media-top">
+                                    <img class="uk-width-1-1 uk-height-max-medium" src="https://via.placeholder.com/600x300" uk-height-viewport="offset-top: true; offset-bottom: 100" alt="">
+                                </div>
+                        <div class="uk-card-body">
+                                    <h6 class="uk-card-title uk-text-bold" style="font-size: 18px;">Judul Penawaran 5</h6>
+                                      <p class="uk-margin-remove-bottom">Mulai Dari Harga</p>
+                                      <span uk-icon="icon: star; ratio: 0.8"></span>
+                                      <span uk-icon="icon: star; ratio: 0.8"></span>
+                                      <span uk-icon="icon: star; ratio: 0.8"></span>
+                                      <span uk-icon="icon: star; ratio: 0.8"></span>
+                                      <span uk-icon="icon: star; ratio: 0.8"></span>
+                        </div>
+            </div>
+
+            <hr class="uk-margin-remove-top uk-margin-small-bottom">
+
+    </div>
+
+</template>
+
+<script>
+module.exports = {
+    data:function(){
+        return{
+            
+        }
+    }
+};
+</script>
