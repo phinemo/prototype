664a6a43da1638ac091003955c6c339beb61af87
phinemo <danarwidiutomo@gmail.com>
2018-09-28 08:49:50 +0700

    add condition button show up

--- components/banneroffer.vue
+++ components/banneroffer.vue
@@ -2,7 +2,7 @@
     <div>
         <div v-for="(url, index) in urls" :key="index" class="uk-inline uk-height-small uk-flex uk-flex-center uk-flex-middle uk-background-cover uk-light uk-margin-bottom" :data-src="url.site" uk-img>
             <h1>{{url.name}}</h1>
-            <a class="uk-button uk-button-default uk-text-small uk-margin-small uk-position-bottom-right" href="">CLICK HERE</a>
+            <a v-if="url.click == true " class="uk-button uk-button-default uk-text-small uk-margin-small uk-position-bottom-right" href="">CLICK HERE</a>
         </div>
     </div>
 
@@ -15,15 +15,18 @@
                 //url for 
                 urls: [{
                     'site': 'https://via.placeholder.com/600x300',
-                    'name':'Tokyo'
+                    'name':'Tokyo',
+                    'click':true
                 },
                 {
                     'site': 'https://via.placeholder.com/600x200',
-                    'name':'Singapore'
+                    'name':'Singapore',
+                    'click':false
                 },
                 {
                     'site': 'https://via.placeholder.com/600x100',
-                    'name':'Sydney'
+                    'name':'Sydney',
+                    'click':false
                 }
             ]
             }
