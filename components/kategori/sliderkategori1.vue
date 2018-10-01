5f50dd88f1dd57b16f7efaeaee996233cd9bc737
Syahrul <syahrulrizal9999@gmail.com>
2018-10-01 08:56:50 +0800

    Fix Sliderkategori1

--- components/kategori/sliderkategori1.vue
+++ components/kategori/sliderkategori1.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="uk-position-relative uk-visible-toggle" uk-slider>
         <ul class="uk-slider-items uk-width-3-4 uk-child-width-1-2@s uk-child-width-1-4@m">
-            <li class="uk-margin-small-right" v-for="(offer, index) in offers" :key="index">
+            <li class="uk-margin-small-right" v-for="(slider1, index) in slider1s" :key="index">
                 <div class=" uk-card uk-card-default uk-box-shadow-material-small uk-border-rounded">
                     <div class="uk-card-media-top">
-                        <a :href="offer.url"><img class="uk-border-rounded" :data-src="offer.image" :alt="offer.title" height="300" width="600"  uk-img></a>
+                        <a :href="slider1.url"><img class="uk-border-rounded" :data-src="slider1.image" :alt="slider1.title" height="300" width="600"  uk-img></a>
                     </div>
                     <div class="uk-card-body uk-padding-small">
-                        <h6 class="uk-card-title uk-text-small uk-text-muted uk-margin-small-bottom"><a :href="offer.url">{{offer.title}}</a></h6>
-                        <p class="uk-text-small uk-margin-small-bottom uk-margin-remove-top">From IDR {{offer.price}}K</p>
-                        <span v-for="rating in offer.ratings" :key="rating" uk-icon="icon: star; ratio: 0.8"></span>
+                        <h6 class="uk-card-title uk-text-small uk-text-muted uk-margin-small-bottom"><a :href="slider1.url">{{slider1.title}}</a></h6>
+                        <p class="uk-text-small uk-margin-small-bottom uk-margin-remove-top">From IDR {{slider1.price}}K</p>
+                        <span v-for="rating in slider1.ratings" :key="rating" uk-icon="icon: star; ratio: 0.8"></span>
                     </div>
                 </div>
             </li>
@@ -22,7 +22,7 @@
     module.exports = {
         data: function () {
             return {
-                offers: [{
+                sider1s: [{
                         title: 'JMt. Batur Sunrise Hike ',
                         image: 'img/batur.jpg',
                         price: '600',
