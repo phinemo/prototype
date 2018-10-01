8f74a15cf4034338ccaf6a74fa0ad8f66ff8da54
Syahrul <syahrulrizal9999@gmail.com>
2018-10-01 08:09:57 +0800

    Fix Url in slider

--- components/homepage/slider.vue
+++ components/homepage/slider.vue
@@ -2,7 +2,7 @@
      <div class="uk-position-relative uk-visible-toggle uk-light" uk-slider>
             <ul class="uk-slider-items uk-width-3-4 uk-child-width-1-2@s uk-child-width-1-4@m">
                 <li v-for="(slider, index) in sliders" :key="index" style="padding-right: 10px;">
-                    <div class="uk-margin-small"><img class="uk-box-shadow-small uk-box-shadow-material-small uk-border-rounded" :data-src="slider.image" :alt="slider.title" height="300" width="600" uk-img></div>
+                    <div class="uk-margin-small"><a :href="slider.url" <img class="uk-box-shadow-small uk-box-shadow-material-small uk-border-rounded" :data-src="slider.image" :alt="slider.title" height="300" width="600" uk-img></a></div>
                     <div class="uk-position-center uk-panel">
                         <h1>{{slider.title}}</h1>
                     </div>
