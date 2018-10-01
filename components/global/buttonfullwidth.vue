70a91b87e0f3d3385a73158441108eadb60b8d8b
phinemo <danarwidiutomo@gmail.com>
2018-10-01 15:34:34 +0700

    make rounded button and fix some link

--- components/global/buttonfullwidth.vue
+++ components/global/buttonfullwidth.vue
@@ -1,5 +1,5 @@
 <template>
-    <a class="uk-button uk-width-1-1 uk-margin-small-bottom" style="color: orange; border: 2px solid orange; background-color: white;" :href="title">{{title}}</a>
+    <a class="uk-button uk-width-1-1 uk-margin-small-bottom uk-border-rounded" style="color: orange; border: 2px solid orange; background-color: white;" :href="title.url">{{title.name}}</a>
 </template>
 <script>
 module.exports = {
