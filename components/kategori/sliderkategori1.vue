ef37f4554b71825c8dff0949ff5f76fc10da4df5
Syahrul <syahrulrizal9999@gmail.com>
2018-10-01 08:49:43 +0800

    Update Sliderkategori1

--- components/kategori/sliderkategori1.vue
+++ components/kategori/sliderkategori1.vue
@@ -1,52 +1,20 @@
 <template>
-    <div>
-        <div uk-slider="center: true">
-            <div class="uk-position-relative uk-visible-toggle uk-light">
-                <ul class="uk-slider-items uk-width-3-4 uk-child-width-1-2@s uk-grid">
-                    <li class="uk-padding-small uk-padding-remove-top uk-padding-remove-bottom uk-padding-remove-right">
-                        <div class="uk-card uk-card-default">
-                            <div class="uk-card-media-top">
-                                <img src="https://via.placeholder.com/600x300" alt="">
-                            </div>
-                        </div>
-                    </li>
-                    <li class="uk-padding-small uk-padding-remove-top uk-padding-remove-bottom uk-padding-remove-right">
-                        <div class="uk-card uk-card-default">
-                            <div class="uk-card-media-top">
-                                <img src="https://via.placeholder.com/600x300" alt="">
-                            </div>
-                        </div>
-                    </li>
-                    <li class="uk-padding-small uk-padding-remove-top uk-padding-remove-bottom uk-padding-remove-right">
-                        <div class="uk-card uk-card-default">
-                            <div class="uk-card-media-top">
-                                <img src="https://via.placeholder.com/600x300" alt="">
-                            </div>
-                        </div>
-                    </li>
-                    <li class="uk-padding-small uk-padding-remove-top uk-padding-remove-bottom uk-padding-remove-right">
-                        <div class="uk-card uk-card-default">
-                            <div class="uk-card-media-top">
-                                <img src="https://via.placeholder.com/600x300" alt="">
-                            </div>
-                        </div>
-                    </li>
-                    <li class="uk-padding-small uk-padding-remove-top uk-padding-remove-bottom uk-padding-remove-right">
-                        <div class="uk-card uk-card-default">
-                            <div class="uk-card-media-top">
-                                <img src="https://via.placeholder.com/600x300" alt="">
-                            </div>
-                        </div>
-                    </li>
-                </ul>
-                <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous
-                    uk-slider-item="previous"></a>
-                <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next
-                    uk-slider-item="next"></a>
-            </div>
-            <button class="uk-button uk-button-primary uk-width-1-1 uk-margin-small-bottom uk-margin-medium-top uk-text-small">Lihat
-                semua hal menarik di Semarang</button>
-        </div>
+    <div class="uk-position-relative uk-visible-toggle" uk-slider>
+        <ul class="uk-slider-items uk-width-3-4 uk-child-width-1-2@s uk-child-width-1-4@m">
+            <li class="uk-margin-small-right" v-for="(offer, index) in offers" :key="index">
+                <div class=" uk-card uk-card-default uk-box-shadow-material-small uk-border-rounded">
+                    <div class="uk-card-media-top">
+                        <a :href="offer.url"><img class="uk-border-rounded" :data-src="offer.image" :alt="offer.title" height="300" width="600"  uk-img></a>
+                    </div>
+                    <div class="uk-card-body uk-padding-small">
+                        <h6 class="uk-card-title uk-text-small uk-text-muted uk-margin-small-bottom"><a :href="offer.url">{{offer.title}}</a></h6>
+                        <p class="uk-text-small uk-margin-small-bottom uk-margin-remove-top">From IDR {{offer.price}}K</p>
+                        <span v-for="rating in offer.ratings" :key="rating" uk-icon="icon: star; ratio: 0.8"></span>
+                    </div>
+                </div>
+            </li>
+        </ul>
+        <hr style="margin-top: 30px; margin-bottom: 10px;">
     </div>
 </template>
 
@@ -54,7 +22,43 @@
     module.exports = {
         data: function () {
             return {
+                offers: [{
+                        title: 'JMt. Batur Sunrise Hike ',
+                        image: 'img/batur.jpg',
+                        price: '600',
+                        ratings: 3,
+                        url:'showoffer.html'
+                    },
+                    {
+                        title: 'Ubud Cycling Tour',
+                        image: 'img/ubud_sepeda.jpg',
+                        price: '700',
+                        ratings: 1,
+                        url:'showoffer.html'
+                    },
+                    {
+                        title: 'Gili Meno Fun Dive Trip',
+                        image: 'img/gili_meno.jpg',
+                        price: '800',
+                        ratings: 5,
+                        url:'showoffer.html'
+                    },
+                    {
+                        title: 'Dieng Plateau Golden Sunrise',
+                        image: 'img/dieng.jpg',
+                        price: '350',
+                        ratings: 1,
+                        url:'showoffer.html'
+                    },
+                    {
+                        title: 'Lava Tour Yogyakarta',
+                        image: 'img/lava_tour.jpg',
+                        price: '1000',
+                        ratings: 4,
+                        url:'showoffer.html'
+                    },
 
+                ]
             }
         }
     };
