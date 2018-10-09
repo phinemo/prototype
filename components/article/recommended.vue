2ac471877df4c3cda968b895c84d93617ffe855a
Syahrul <syahrulrizal9999@gmail.com>
2018-10-09 07:55:10 +0700

    Update padding artikel

--- components/article/recommended.vue
+++ components/article/recommended.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="uk-container uk-card uk-card-body uk-padding-remove-bottom">
+<div class="uk-container uk-card uk-card-body uk-padding-remove-bottom uk-padding-remove-top">
         <h5 class="black-text"><b>REKOMENDASI</b></h5>
         <ul class="uk-list-divider uk-link-reset uk-text-medium uk-text-capitalize" style="list-style:decimal-leading-zero; color:#444444">
             <li>
